32bc74ad39af11a33374fdc51887e81b46816f96
Salvador Hernández <zhavaman@gmail.com>
2017-07-31 17:50:24 -0500

    #1 add script to change life word in section 1 title

---
+++ src/js/s1-title-anim.js
@@ -0,0 +1,6 @@
+var sOneTitle = document.getElementById('section_one-title');
+var span = document.getElementById('topink');
+
+sOneTitle.addEventListener('mouseover', function () {
+  span.classList.add('lifePink')
+});
