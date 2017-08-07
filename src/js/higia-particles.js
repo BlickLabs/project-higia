2ee5e08a86c7d47b0a4632addc7b7f18fa0752e9
Salvador Hernández <zhavaman@gmail.com>
2017-08-07 08:44:48 -0500

    #1 unable particles to develop mobile version

--- src/js/higia-particles.js
+++ src/js/higia-particles.js
@@ -2,7 +2,7 @@ if (window.location.pathname === '/index.html' || window.location.pathname === '
   var partConfig = 'js/higia-particles_config.json'
 
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS.load('home-a-particles-container', partConfig);
-  particlesJS.load('home-b-particles-container', partConfig);
+  //particlesJS.load('home-a-particles-container', partConfig);
+  //particlesJS.load('home-b-particles-container', partConfig);
   
 }
