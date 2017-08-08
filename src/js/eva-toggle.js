72306457175477c82af7cccb316ff2d92b05d9f3
Salvador Hernández <zhavaman@gmail.com>
2017-08-08 12:31:37 -0500

    #1 add resize functionality to eva hidden description button

--- src/js/eva-toggle.js
+++ src/js/eva-toggle.js
@@ -5,5 +5,17 @@ if (window.location.pathname === '/index.html' || window.location.pathname === '
   $evaButton.addEventListener('click', function () {
     $hiddenDescription.classList.toggle('active');
     $evaButton.classList.toggle('active');
+    evaFeatures ()
+    
+    if ($hiddenDescription.classList.contains('active')) {
+      var absContainerHeight = document.getElementById('absolute-container-a').clientHeight;
+      partContainerA.style.height = (absContainerHeight + 500)+'px';
+    } else {
+      var absContainerHeight = document.getElementById('absolute-container-a').clientHeight;
+      partContainerA.style.height = (absContainerHeight - 500)+'px';
+    }
+    
+    partContainerA.style.transition = 'height .8s ease';
+
   });
 }
