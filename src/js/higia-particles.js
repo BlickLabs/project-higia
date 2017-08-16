1feba6985d47d1a4b3fec7e0186f939660076aa0
Salvador Hernández <zhavaman@gmail.com>
2017-08-16 07:26:12 -0500

    #75 add index-es to all js files

--- src/js/higia-particles.js
+++ src/js/higia-particles.js
@@ -1,4 +1,4 @@
-if (window.location.pathname === '/index.html' || window.location.pathname === '/' || window.location.pathname === '/project-higia/index.html' || window.location.pathname === '/project-higia/') {
+if (window.location.pathname === '/index.html' || window.location.pathname === '/index-es.html' || window.location.pathname === '/' || window.location.pathname === '/project-higia/index.html' || window.location.pathname === '/project-higia/') {
   var partConfig = 'js/higia-particles_config.json'
 
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
