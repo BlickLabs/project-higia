0b3fdd08c27b0ca21651be3917caa639bf00b139
soycamis <camiilososa@gmail.com>
2017-08-18 19:27:54 -0500

    #1 animation in logo and part of text

--- src/js/s1-title-anim.js
+++ src/js/s1-title-anim.js
@@ -1,8 +1,11 @@
 if (indexLocation) {
-  var sOneTitle = document.getElementById('section_one-title');
-  var span = document.getElementById('topink');
 
-  sOneTitle.addEventListener('mouseover', function () {
-    span.classList.add('lifePink')
+  var animated = $('.section_one');
+  var logo = $('.section_one-mask-container img');
+  var span = $('#topink');
+
+  animated.hover(function() {
+    logo.addClass('logoPink')
+    span.addClass('lifePink');
   });
 }
