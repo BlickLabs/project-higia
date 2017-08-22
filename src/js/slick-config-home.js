dfe514721356485cc51b19c2d260d5e0427071fe
soycamis <camiilososa@gmail.com>
2017-08-22 17:54:29 -0500

    #60 add file, delete for error!

---
+++ src/js/slick-config-home.js
@@ -0,0 +1,30 @@
+$('.section_four-slider-container').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: '0px',
+  prevArrow: ' <button class="arrow-slider arrow-left"><img class="arrow" src="img/slider-arrow/left.svg" alt=""></button>',
+  nextArrow: ' <button class="arrow-slider arrow-right"><img class="arrow" src="img/slider-arrow/right.svg" alt=""></button>'
+});
+
+window.addEventListener('resize', homePressCarroussel);
+
+function homePressCarroussel() {
+  if (window.matchMedia("(max-width: 500px)").matches) {
+    $('.articles').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      centerMode: true,
+      centerPadding: '0px',
+      prevArrow: '<button class="slick-prev slick-arrow buttont buttont-l"><img class="" src="img/slider-arrow/left.svg" alt=""></button>',
+      nextArrow: '<button class="slick-next slick-arrow buttont buttont-r"><img class="" src="img/slider-arrow/right.svg" alt=""></button>'
+    });
+
+    evaFeatures();
+
+  } else {
+    $('.articles').slick('unslick');
+  }
+}
