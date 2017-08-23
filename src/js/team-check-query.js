7a56b2f492b8e8cb36aaa775861d387dbecc4fff
soycamis <camiilososa@gmail.com>
2017-08-23 12:47:07 -0500

    #35 error correction

--- src/js/team-check-query.js
+++ src/js/team-check-query.js
@@ -3,14 +3,6 @@
 	// Function to check if it is on mobile and activate the modals or visiversa.
 	var mediaquery = window.matchMedia("(max-width: 876px)");
 	var team = $('.team-photo-img');
-	var teamUser = [
-		'julian',
-		'alejandro',
-		'raymundo',
-		'fernando',
-		'antonio',
-		'jose'
-	];
 
 	// Removes the activation of the modals if it opens in the desktop.
 	if (!mediaquery.matches) {
@@ -20,7 +12,25 @@
 	}
 
 	// A manager is added to check the width of the screen
-	function handleOrientationChange(mediaquery, teamUser) {
+	function handleOrientationChange(mediaquery) {
+		var team = $('.team-photo-img');
+		var teamUser = [
+			'julian',
+			'alejandro',
+			'raymundo',
+			'fernando',
+			'antonio',
+			'jose',
+			'martin',
+			'rebeca',
+			'vicente',
+			'ta-antonio',
+			'ta-felicia',
+			'ta-julio',
+			'ta-raymond',
+			'ta-isaac',
+			'ta-erickPonce'
+		];
 	  if (mediaquery.matches) {
 			team.each(function (index, value) {
 				value.setAttribute('data-remodal-target', teamUser[index])
