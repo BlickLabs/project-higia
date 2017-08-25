015e2f2cebc153b51b505f4c6c8c3e8b0f4a4d88
soycamis <camiilososa@gmail.com>
2017-08-25 18:36:22 -0500

    #127 change url done

--- src/js/allies-hover.js
+++ src/js/allies-hover.js
@@ -4,9 +4,9 @@
   var imgApple = $('#apple img');
 
   apple.hover(function() {
-    imgApple.attr('src', '/img/allies/apple.png').fadeIn();
+    imgApple.attr('src', 'img/allies/apple.png').fadeIn();
   }, function() {
-    imgApple.attr('src', '/img/allies/apple2.png').fadeIn();
+    imgApple.attr('src', 'img/allies/apple2.png').fadeIn();
   });
 
 
