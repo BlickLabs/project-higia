2f4db244d249aa0793bbf48ff73cb03c12403484
soycamis <camiilososa@gmail.com>
2017-09-08 16:59:54 -0500

    154 add pathname to index

--- src/js/aq_index_location.js
+++ src/js/aq_index_location.js
@@ -1,6 +1,7 @@
 var indexLocation;
 if  ( 
       window.location.pathname === '/' ||
+      window.location.pathname === '/index' ||
       window.location.pathname === '/index-es' ||
       window.location.pathname === '/project-higia/' ||
       window.location.pathname === '/project-higia/index' ||
