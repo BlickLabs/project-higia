40e722492226986ed837319b2899b79c2f2a953d
soycamis <camiilososa@gmail.com>
2017-09-27 11:00:05 -0500

    #157 deactivating logo animation in mobil

--- src/js/s1-title-anim.js
+++ src/js/s1-title-anim.js
@@ -3,9 +3,15 @@ if (indexLocation) {
   var animated = $('.section_one');
   var logo = $('.section_one-mask-container');
   var span = $('#topink');
+  var mediaquery = window.matchMedia("(max-width: 550px)");
 
-  animated.hover(function() {
-    logo.addClass('section_one-mask-container2');
-    span.addClass('lifePink');
-  });
+  // animations are deactivated for mobiles
+  if (!mediaquery.matches) {
+    animated.hover(function() {
+      logo.addClass('section_one-mask-container2');
+      span.addClass('lifePink');
+    });
+  }
+
+  
 }
